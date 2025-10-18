c46304a51fd14c7873d7fba1f34f555fe91fff11
lanttu1243 <lasse.kukkula@gmail.com>
2025-10-18 21:56:12 +0300

    Fix rebase issues with migrations

--- packages/ilmomasiina-backend/src/models/migrations/0008-add-numeric-price.ts
+++ packages/ilmomasiina-backend/src/models/migrations/0008-add-numeric-price.ts
@@ -1,7 +1,7 @@
 import { defineMigration } from "./util";
 
 export default defineMigration({
-  name: "0006-add-numeric-price",
+  name: "0008-add-numeric-price",
   async up({ context: { sequelize } }) {
     const query = sequelize.getQueryInterface();
     await query.addColumn("event", "numPrice", {
