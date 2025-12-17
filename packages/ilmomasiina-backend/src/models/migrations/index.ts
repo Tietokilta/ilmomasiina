38a8b55b2d4194d32260a4df78c42480c98a01a5
Petrus Asikainen <petrus.asikainen@aalto.fi>
2025-12-18 01:00:35 +0200

    fix: update migration filenames

--- packages/ilmomasiina-backend/src/models/migrations/index.ts
+++ packages/ilmomasiina-backend/src/models/migrations/index.ts
@@ -9,10 +9,8 @@ import _0004_answers_to_json from "./0004-answers-to-json";
 import _0005_add_indexes from "./0005-add-indexes";
 import _0006_json_datatype from "./0006-json-datatype";
 import _0007_add_languages from "./0007-add-languages";
-import _0008_add_price_to_quota from "./0008-add-price-to-quota";
-import _0009_add_price_to_question_options from "./0009-add-price-to-question-options";
-import _0010_add_price_to_signup from "./0010-add-price-to-signup";
-import _0011_add_payment_table from "./0011-add-payment-table";
+import _0008_add_price_fields from "./0008-add-price-fields";
+import _0009_add_payment_table from "./0009-add-payment-table";
 
 const migrations: RunnableMigration<Sequelize>[] = [
   _0000_initial,
@@ -23,10 +21,8 @@ const migrations: RunnableMigration<Sequelize>[] = [
   _0005_add_indexes,
   _0006_json_datatype,
   _0007_add_languages,
-  _0008_add_price_to_quota,
-  _0009_add_price_to_question_options,
-  _0010_add_price_to_signup,
-  _0011_add_payment_table,
+  _0008_add_price_fields,
+  _0009_add_payment_table,
 ];
 
 export default migrations;
